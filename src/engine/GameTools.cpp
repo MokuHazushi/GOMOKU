37b14025567b143cd325184396dc247a203b44d3
bastien <mokuhazushi.programming@gmail.com>
2018-03-01 12:44:24 +0900

    [UPDATE] Refractored GameTools functions

--- src/engine/GameTools.cpp
+++ src/engine/GameTools.cpp
@@ -14,8 +14,13 @@ void GameTools::getString(Board *board, StoneString *tempRes,
 	std::vector<std::pair<int,int>> *stones = tempRes->getString();
 	stones->push_back(std::make_pair(x,y));
 
-	if (x == 0) {
-		GameTools::getString(board, tempRes, x+1, y, type);
+	if (x == 0 || x == 18) {
+		if (x == 0) {
+			GameTools::getString(board, tempRes, x+1, y, type);
+		}
+		else {
+			GameTools::getString(board, tempRes, x-1, y, type);
+		}
 
 		if (y == 0) {
 			GameTools::getString(board, tempRes, x, y+1, type);
@@ -29,29 +34,13 @@ void GameTools::getString(Board *board, StoneString *tempRes,
 		}
 		return;
 	}
-	else if (x == 18) {
-		GameTools::getString(board, tempRes, x-1, y, type);
-
+	else if (y == 0 || y == 18) {
 		if (y == 0) {
 			GameTools::getString(board, tempRes, x, y+1, type);
 		}
-		else if (y == 18) {
-			GameTools::getString(board, tempRes, x, y-1, type);
-		}
 		else {
-			GameTools::getString(board, tempRes, x, y+1, type);
 			GameTools::getString(board, tempRes, x, y-1, type);
 		}
-		return;
-	}
-	else if (y == 0) {
-		GameTools::getString(board, tempRes, x, y+1, type);
-		GameTools::getString(board, tempRes, x-1, y, type);
-		GameTools::getString(board, tempRes, x+1, y, type);
-		return;
-	}
-	else if (y == 18) {
-		GameTools::getString(board, tempRes, x, y-1, type);
 		GameTools::getString(board, tempRes, x-1, y, type);
 		GameTools::getString(board, tempRes, x+1, y, type);
 		return;
@@ -78,8 +67,13 @@ int GameTools::countStoneLiberties(Board *board, int x, int y)
 {
 	int total = 0;
 
-	if (x == 0) {
-		if (board->isEmpty(x+1, y)) total++;
+	if (x == 0 || x == 18) {
+		if (x == 0) {
+			if (board->isEmpty(x+1, y)) total++;
+		}
+		else {
+			if (board->isEmpty(x-1, y)) total++;
+		}
 
 		if (y == 0) {
 			if (board->isEmpty(x, y+1)) total++;
@@ -93,29 +87,13 @@ int GameTools::countStoneLiberties(Board *board, int x, int y)
 		}
 		return total;
 	}
-	else if (x == 18) {
-		if (board->isEmpty(x-1, y)) total++;
-
+	else if (y == 0 || y == 18) {
 		if (y == 0) {
 			if (board->isEmpty(x, y+1)) total++;
 		}
-		else if (y == 18) {
-			if (board->isEmpty(x, y-1)) total++;
-		}
 		else {
-			if (board->isEmpty(x, y+1)) total++;
 			if (board->isEmpty(x, y-1)) total++;
 		}
-		return total;
-	}
-	else if (y == 0) {
-		if (board->isEmpty(x, y+1)) total++;
-		if (board->isEmpty(x-1, y)) total++;
-		if (board->isEmpty(x+1, y)) total++;
-		return total;
-	}
-	else if (y == 18) {
-		if (board->isEmpty(x, y-1)) total++;
 		if (board->isEmpty(x-1, y)) total++;
 		if (board->isEmpty(x+1, y)) total++;
 		return total;
